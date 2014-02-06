14dc3c5ced1699592aeb53dbc70480a95b76e5f1
anubiann00b <skraman1999@gmail.com>
2014-02-06 07:50:51 -0800

    Need to merge in stuff from school.

--- src/edit/MainScreen.java
+++ src/edit/MainScreen.java
@@ -13,9 +13,10 @@ public class MainScreen implements Screen {
         menuOptions.add(new MenuOption("Edit"));
         menuOptions.add(new MenuOption("Search"));
         menuOptions.add(new MenuOption("Options"));
-
+        menuOptions.add(new MenuOption("Help",40));
     }
     
+    @Override
     public void displayOutput(AsciiPanel terminal) {
         displayMenuBar(terminal);
         terminal.writeCenter("A DOS Edit clone made in Java.",5);
@@ -24,13 +25,15 @@ public class MainScreen implements Screen {
     private void displayMenuBar(AsciiPanel terminal) {
         terminal.setCursorPosition(0,0);
         for (MenuOption o : menuOptions) {
-            terminal.write("   ",AsciiPanel.black,AsciiPanel.white);
+            for (int i=0;i<o.spaces();i++)
+                terminal.write(' ',AsciiPanel.black,AsciiPanel.white);
             terminal.write(o.getName(),AsciiPanel.black,AsciiPanel.white);
         }
-        while (terminal.getCursorX()<terminal.getWidthInCharacters()-1)
-            terminal.write(" ",AsciiPanel.black,AsciiPanel.white);
+        for (int i=0;i<3;i++)
+            terminal.write(' ',77+i,0,AsciiPanel.black,AsciiPanel.white);
     }
 
+    @Override
     public Screen respondToUserInput(KeyEvent key) {
         return this;
     }
